552129509038399438a3a6bd01d42b2d3544bfa6
swarneswaran-it19 <swarneswaran.it19@bitsathy.ac.in>
2019-08-21 15:40:02 +0530

    first commit

---
+++ tables1.php
@@ -0,0 +1,15 @@
+<html>
+<head><title>adding two numbers</title></head>
+<body>
+<?php
+$a=5;
+echo"<table border=2 >";
+for($s=1;$s<=30;$s++)
+{
+$c=$s*$a;
+echo"<tr><td>$s</td><td>x</td><td>$a</td><td>=</td><td>$c</td></tr>";
+//echo"</table>";
+}
+?>
+</body>
+</html>
